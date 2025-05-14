d4f5b56a1f5ca79ae3ee079e7667777d20eaa1ec
ghxfarr_ <abdillah.atansyah@student.president.ac.id>
2025-05-14 16:00:31 +0700

    Delete TaskManager directory

--- TaskManager/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-javac -cp "lib/gson-2.10.1.jar" -d out src/controller/*.java src/model/*.java src/view/*.java
-echo "✅ Compilation complete."
--- TaskManager/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -cp "out:lib/gson-2.10.1.jar" view.TaskUI
